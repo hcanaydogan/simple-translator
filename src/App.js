bb22a8c7db203955624c4a11f90696d4eff2f29d
H Can Aydogan <h.canaydogan@azerion.com>
2022-06-19 18:36:51 +0300

    initialize app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="App">
+      App
+      <div className="icon__replay"></div>
+      <div className="icon__mic"> </div>
+      <div className="icon__hearing"> </div>
+      <div className="icon__translate"> </div>
+      <div className="icon__backup"> </div>
+      <div className="icon__backup"> </div>
+    </div>
+  );
+}
+
+export default App;
